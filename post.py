3ac0c9d7b3ee9915906d29b9641560cb22a4f980
Robert Schriver <schriver@rancor.csh.rit.edu>
2010-09-23 05:25:21 -0400

    Added weighted random choice based off of the number of posts a word starts. Twitter functionality still doesnt work

--- post.py
+++ post.py
@@ -2,7 +2,22 @@ import pickle
 import oauthtwitter
 import random
 
-data_map = pickle.load(open("twitter-data"))
+def select_weighted():
+	total = 0
+	for key in data_map:
+		total += data_map[key][1] * 2
+	
+	for key in data_map:
+		rand_num = random.uniform(0,1)
+		acc_prob = 1.0 / (total - (data_map[key][1]*2)^2)
+		if rand_num <= acc_prob:
+			return key
+
+	return random.choice(data_map.keys())
+
+script_directory = "/users/u16/schriver/projects/personal/mark_ovcott/"
+
+data_map = pickle.load(open(script_directory + "twitter-data"))
 
 access_token = "191210353-J9YTr34SIo0J8gFlmSlxJxjcFlkb0Ucw86NQSnvw"
 access_token_secret = "OiG7X4yykQBWDUjsrFxwYPmCxhoZoLH9k1FTnCDndE"
@@ -10,24 +25,41 @@ access_token_secret = "OiG7X4yykQBWDUjsrFxwYPmCxhoZoLH9k1FTnCDndE"
 consumer_token = "FAIfyVsiSfdGlbyu4eSA"
 consumer_token_secret = "jHX6AZGoc5aCv6ueB0DDmvvjSY1U2njEQsD97SkVE"
 
+api = oauthtwitter.OAuthApi(consumer_token, consumer_token_secret)
+
+#request_token = api.getRequestToken()
+
+#access_token = api.getAccessToken()
+
+#api = oauthtwitter.OAuthApi(consumer_token, consumer_token_secret, access_tiken)
+
 api = oauthtwitter.OAuthApi(consumer_token, consumer_token_secret, access_token , access_token_secret)
 
-status = ""
+#status = ""
 
-key = random.choice(data_map.keys())
+key = select_weighted()
 status = key[0] + " " + key[1]
 
+print key
+print data_map[key][1]
 
-while key in data_map:
-	value = random.choice(data_map[key])
+while key in data_map and len(status) < 140:
+	value = random.choice(data_map[key][0])
 	status += " " + value
 	key = (key[1], value)
 
-api.UpdateStatus(status)
-
 count = 0
+
+
 for pair in data_map:
 	count += len(data_map[pair])
+	#print pair
+	#print data_map[pair][0]
+	#print data_map[pair][1]
 
+print status
 print len(data_map)
 print count
+
+#api.UpdateStatus(status)
+
