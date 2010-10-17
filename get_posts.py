fcfdddac5e1bf83d5e1411e3526ddc6bf064257a
Robert Schriver <schriver@rancor.csh.rit.edu>
2010-10-17 16:14:21 -0400

    Added variable order counts, comments, improved regex

--- get_posts.py
+++ get_posts.py
@@ -1,20 +1,26 @@
 import twitter
-import pickle
+import cPickle as pickle
 import os
 import HTMLParser
 import re
+import sys
+import unicodedata
+
+order = int(sys.argv[1])
+values = int(sys.argv[2])
 
 num_results = 100
-script_dir = "/users/u16/schriver/projects/personal/mark_ovcott/"
-
-
 search_terms = ['#tcot' , '#teaparty' , '#wreckingcrew' , '#twisters' , '#gop' , '#ocra' , '#sgp']
 
-data_filename = "twitter-data"
+script_dir = "/users/u16/projects/personal/mark-ovcott/"
+
+data_filename = "twitter_data"
 last_search_filename = "last_search_time"
+start_sen_filename = "start_sen_count"
 
 data_filename_abs = script_dir + data_filename
 last_search_filename_abs = script_dir + last_search_filename
+start_sen_filename_abs = script_dir + start_sen_filename
 
 
 if not os.path.exists(data_filename_abs):
@@ -23,9 +29,14 @@ else:
 	data_map = pickle.load(open(data_filename_abs))
 
 if not os.path.exists(last_search_filename_abs):
-	last_search_time_map = {}
+	last_search_time_map = dict()
 else:
 	last_search_time_map = pickle.load(open(last_search_filename_abs))
+
+if not os.path.exists(start_sen_filename_abs):
+	start_sen_list = list()
+else:
+	start_sen_list = pickle.load(open(start_sen_filename_abs))
 
 api = twitter.Api()
 html_parser = HTMLParser.HTMLParser()
@@ -39,54 +50,57 @@ for search_term in search_terms:
 	else:
 		statuses = api.GetSearch(search_term , None , None ,  num_results)
 	
-	#print len(statuses)
-	
 	last_search_time_map[search_term] = statuses[-1].GetId()
 	
-	for status in statuses:
-		if status.GetText() not in previous_statuses and 'http://' not in status.GetText() and 'www.' not in status.GetText() and '.com' not in status.GetText():
+	for status in statuses:		
+		if status.GetText() not in previous_statuses: 
+			#Make sure we don't add duplicate posts
 			previous_statuses.append(status.GetText())
+			
+			#lets first convert the text to something we can deal with
 			text = status.GetText()
 			text = html_parser.unescape(text)
-			#some regex's to fix words attached to punctuation
-			text = re.sub('[\*\[\]\(\)\<\>]', '' , text)
-			text = re.sub('(\w)[\|\-\=\/\\\](\w)' , '\1 \2', text)
+			unicodedata.normalize("NFKD" , text).encode("ascii", "ignore")
+			text = text.lower()
 			
-			#text = re.sub('([\(\{\[\<\"\*])(\w))', '\1 \2' , text)
-			#text = re.sub('(\w)([\]\}\)\>\"\*])' , '\1 \2' , text)
+			#remove URL's
+			text = re.sub("(http://)?(www\.)?\w+\.\w{2,3}.*\s", " " , text)
+			#Remove any doubled non word characters
+			text = re.sub("(\W)\W+" , "\1" , text)
+			#Remove opening/closing punctuation so we don't have random open parens etc
+			text = re.sub("[\*\[\]\(\)\<\>]", "" , text)
+			#replace current word seperators with spaces
+			text = re.sub("(\w)[\|\-\=\/\\\](\w)" , "\1 \2", text)
+			#Put a space between the end of sentences
+			text = re.sub("(\w)([\.\?\!\,\:\;])(\w)", "\1\2 \3", text) 
 
-			text = re.sub('"', '', text)
-
-			text = re.sub('([\.\?\!\,\:\;])(\w)', '\1 \2', text) 
 			words = text.split()
-			for i in range( len(words) - 2):
-				word_one = words[i].lower()
-				word_two = words[i+1].lower()
-				word_three = words[i+2].lower()
-					
 
-				key = (word_one , word_two)
-				value = word_three				
+			for i in range( len(words) - (order + values)):
+				key = []
+				value = []
+
+				for j in range(order):
+					key.append(words[i + j])
 				
-				start_sen_count = len(data_map) / 1000 if i == 0 else 0			
-						
-				if word_one == word_two and word_two == word_three: continue
+				for j in range(order , order + values):
+					value.append(words[i+j])
+	
+				key = tuple(key)
+				value = tuple(value)		
 
+				if i == 0:
+					start_sen_list.append(key)
+						
 				if key not in data_map:
-					data_map[key] = [[], 0]
-				#else:
-				#	if '#' in value:
-				#		count = 0
-				#		for curr_list_item in data_map[key][0]:
-				#			if '#' in curr_list_item:
-				#				count += 1
-				#
-				#		if count > len(curr_list_item) / 2:
-				#			continue
-							
-				data_map[key][0].append(value)
-				data_map[key][1] += start_sen_count
+					data_map[key] = []
+				
+				data_map[key].append(value)
+
+
+print data_map
 
 
 pickle.dump(data_map , open(data_filename_abs , "w"))
 pickle.dump(last_search_time_map , open(last_search_filename_abs, "w"))
+pickle.dump(start_sen_list, open(start_sen_filename_abs , "w"))
